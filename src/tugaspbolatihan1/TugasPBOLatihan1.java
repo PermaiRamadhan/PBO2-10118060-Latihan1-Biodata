e3a13a811abbc8858d0db6fdbad6f027c6d7dff1
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-09-26 14:34:47 +0700

    First Commit

---
+++ src/tugaspbolatihan1/TugasPBOLatihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbolatihan1;
+
+/**
+ *
+ * @author 
+ * Nama : Permai Ramadhan
+ * Kelas : IF 2
+ * NIM : 10118060
+ * Deskripsi Program : Perintah Menampilkan Value ke Layar
+ */
+public class TugasPBOLatihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Permai Ramadhan");
+        System.out.println("ALAMAT\t\t: Jalan Kubangsari 11 no 12");
+        System.out.println("TEMPAT LAHIR\t\t: Belitung");
+        System.out.println("TANGGAL LAHIR\t\t: 03 Desember 2000");
+        System.out.println("--------------------");
+        System.out.println("Hobi\t\t: Bersepeda dan Main Game");
+        System.out.println("Makes\t\t: Mie Ayam");
+        System.out.println("Mikes\t\t: Es Campur");
+    }
+    
+}
